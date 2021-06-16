1a9b522e3edbf018f15eaab52560e13fe834da25
Erik Svensson <eosvensson@gmail.com>
2021-06-16 11:26:05 +0200

    Added some tests

--- src/test/java/se/lexicon/g36_spring_di/data_access/StudentDAOListImplTest.java
+++ src/test/java/se/lexicon/g36_spring_di/data_access/StudentDAOListImplTest.java
@@ -17,23 +17,62 @@ import static org.junit.jupiter.api.Assertions.*;
 @ContextConfiguration(classes = AppConfig.class)
 public class StudentDAOListImplTest {
 	
-	@Autowired private StudentDAOListImpl testObject;	
+	@Autowired private StudentDAOListImpl testObject;
+	
+	private Student testStudent;
 	
 	@BeforeEach
 	void setup() {
-		testObject.clear();		
+		testObject.clear();
+		testStudent = new Student(1, "Test testsson");
+		testObject.save(testStudent);
 	}
 	
 	@Test
 	@DisplayName("Given Student student successfully create successfully adds student")
 	void save() {
-		Student student = new Student(1, "Erik Svensson");
+		Student student = new Student(2, "Erik Svensson");
 		
 		testObject.save(student);
 		
-		Student persisted = testObject.find(1);
+		Student persisted = testObject.find(2);
 		
 		assertEquals(persisted, student);
 	}
+	
+	@Test
+	@DisplayName("Adding Student student twice create will not add same object twice")
+	void save_duplicate() {
+		Student student = new Student(2, "Erik Svensson");
+		
+		testObject.save(student);
+		testObject.save(student);
+		
+		
+		
+		
+	}
+	
+	@Test
+	@DisplayName("Given id of 1 findById return expected object")
+	void findById() {
+		int id = 1;
+		
+		Student result = testObject.find(id);
+		
+		assertNotNull(result);
+		assertEquals(id, result.getId());		
+	}
+	
+	@Test
+	@DisplayName("given id of 2 findById throws RuntimeException")
+	void findById_throws_RuntimeException() {		
+		assertThrows(
+				RuntimeException.class,
+				() -> testObject.find(2)
+		);		
+	}
+	
+	
 
 }
